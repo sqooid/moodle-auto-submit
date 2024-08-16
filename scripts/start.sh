dc428eed435bdcede33dff6fee4b61231adb9a24
Lucas <git@sl.thesqooid.com>
2024-08-16 19:53:44 +1000

    fix run script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-npx playwright install-deps
-npx playwright install chromium
+npx playwright install-deps chromium
+su node -c "npx playwright install chromium"
 su node -c "node index.js"
